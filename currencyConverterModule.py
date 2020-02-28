37a00c932ae3bc869d9d2a099beccc26e265746d
hbaina <bAsit903%%>
2020-02-28 22:35:32 +0100

    My first commit

---
+++ currencyConverterModule.py
@@ -0,0 +1,44 @@
+#======NGN><USD CURRENCIES
+def NGN_USD(amount):
+   if amount > 0:
+      return amount / 358
+def USD_NGN(amount):
+   if amount > 0:
+      return amount * 358
+#======NGN><GBP CURRENCIES
+def NGN_GBP(amount):
+   if amount > 0:
+      return amount / 467
+def GBP_NGN(amount):
+   if amount > 0:
+      return amount * 467
+#======NGN><EUR CURRENCIES      
+def NGN_EUR(amount):
+   if amount > 0:
+      return amount / 386
+def EUR_NGN(amount):
+   if amount > 0:
+     return amount * 386
+#======USD><GBPCURRENCIES
+def USD_GBP(amount):
+   if amount > 0:
+      return amount * 0.779879
+def GBP_USD(amount):
+   if amount > 0:
+      return amount * 1.28225
+#======USD><EUR CURRENCIES
+def USD_EUR(amount):
+   if amount > 0:
+      return amount * 0.910047
+def EUR_USD(amount):
+   if amount > 0:
+      return amount * 1.09884
+#======GBP><EUR CURRENCIES
+def GBP_EUR(amount):
+   if amount > 0:
+      return amount * 1.16996
+def EUR_GBP(amount):
+   if amount > 0:
+      return amount * 0.855070
+
+
